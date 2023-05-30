3e0b73bffc19cd0204682623b573af08797b33cb
@pmi <paolo.milano@isi.it>
2023-05-30 17:33:17 +0200

    test file access

--- .github/scripts/verify_access.py
+++ .github/scripts/verify_access.py
@@ -1,3 +1,17 @@
+import os
+
+
+def run ():
+    print ("Running check")
+    jsonInputFile = "../assets/repo_access_mapping.json"
+    with open(jsonInputFile) as f_json_input:
+        j_in = json.load(f_json_input)
+        if j_in == None :
+            print("Failed to open input file")
+        else :
+            print("Input file opened: {}".format(j_in))
+
+
 
 if __name__ == "__main__":
     print ("Testing script")
