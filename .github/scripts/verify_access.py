8b6f15fa7f7f92f6d5c237274c600e1391612779
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-07-18 18:46:26 +0200

    Update verify_access.py

--- .github/scripts/verify_access.py
+++ .github/scripts/verify_access.py
@@ -3,28 +3,13 @@ import json
 import sys
 
 #
-def getEnvInfo ():
-
-    env_file = os.getenv('GITHUB_ENV')
-
-    test_var = os.getenv('Test_Parameter')
-
-    print ("Test parameter from env: {}".format(test_var))
-
-    with open(env_file, "a") as myfile:
-        myfile.write("MY_VAR=MY_FLUCAST_VALUE")
-        print ("MyFile: {}".format(myfile))
-
-#
 def getInputFromOutput():
     env_file = os.getenv('GITHUB_OUTPUT')
 
+    local_var = os.getenv("local_test_var")
+
+    print ("LOCAL VAR: {}".format(local_var))
     
-    # actor = os.getenv("gh-actor")
-    # repo = os.getenv("gh-repo")
-    # pr_number = os.getenv("pr-number")
-    # changes = os.getenv("all-changes")
-    # test_p = os.getenv("test-p")
     with open (env_file, 'r') as gh_env_file:
         print ("OUTPUT content read START>>>>>>>>>> ")
         print (gh_env_file.read())
@@ -33,30 +18,8 @@ def getInputFromOutput():
 
 
 #
-def getInputFromEnv ():
-    
-    env_file = os.getenv('GITHUB_ENV')
-
-    
-    # actor = os.getenv("gh-actor")
-    # repo = os.getenv("gh-repo")
-    # pr_number = os.getenv("pr-number")
-    # changes = os.getenv("all-changes")
-    # test_p = os.getenv("test-p")
-    with open (env_file, 'r') as gh_env_file:
-        print ("Env content read START>>>>>>>>>> ")
-        print (gh_env_file.read())
-        print ("Env content read STOP >>>>>>>>>> ")
-
-    # print ("Input info -> \nactor: {0}, \nrepo: {1}, \npr num: {2}, \nchanges: {3}".format(actor, repo, pr_number, changes))
-
-
-#
 def run (jsonInputFile):
 
-    print ("Running check")
-
-    getInputFromEnv()
     getInputFromOutput()
 
     with open(jsonInputFile) as f_json_input:
@@ -84,7 +47,6 @@ if __name__ == "__main__":
     # print ("Input data: \n {}".format(input_data_json))
 
     runningPath = os.path.dirname(__file__)
-
     jsonInputFile = os.path.join(runningPath, '..', 'assets', 'repo_access_mapping.json')
 
     print ("Running path: {0}, file: {1}".format(runningPath, jsonInputFile))
